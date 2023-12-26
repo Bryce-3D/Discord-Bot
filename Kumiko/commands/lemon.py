94bb227b9d00c77468477130efd8a7e4745b702b
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2023-12-27 01:02:57 +0800

    Improve %lemon

--- Kumiko/commands/lemon.py
+++ Kumiko/commands/lemon.py
@@ -1,6 +1,26 @@
 from ..bot_config import bot, commands
 
+#Adjustable parameters of the 🍋 wave
+cap   = 8
+curve = [0,1,4,7,8,7,4,1]
+width = 3
+count = 3
 
+lemon_wave  = ''
+for i in curve:
+    s  = ' '*i
+    s += '🍋'*3
+    s += ' '*(cap-i)
+    s += '\n'
+    lemon_wave += s
+
+lemon_wave *= count
+
+# lemon_wave  = '🍋🍋🍋      \n'
+# lemon_wave += '   🍋🍋🍋   \n'
+# lemon_wave += '      🍋🍋🍋\n'
+# lemon_wave += '   🍋🍋🍋   \n'
+# lemon_wave *= 5
 
 @bot.command()
 async def lemon(ctx:commands.Context, n:int=5) -> None:
@@ -8,9 +28,5 @@ async def lemon(ctx:commands.Context, n:int=5) -> None:
     #Prevent over-abuse
     n = min(n,100)
 
-    lemon  = '🍋🍋🍋    \n'
-    lemon += '  🍋🍋🍋  \n'
-    lemon += '    🍋🍋🍋\n'
-    lemon += '  🍋🍋🍋  \n'
     for i in range(n):
-        await ctx.send(lemon)
+        await ctx.send(lemon_wave)

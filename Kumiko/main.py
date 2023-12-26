e74c9c98def71d93c149be7af2a6390b2e2aeb31
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2023-12-27 01:08:45 +0800

    Clean Kumiko/main.py and add a test function

--- Kumiko/main.py
+++ Kumiko/main.py
@@ -1,39 +1,25 @@
-import discord
-from discord.ext import commands
-
+'''
+Credits to 
+    https://github.com/Rapptz/discord.py/blob/v1.7.3/examples/basic_bot.py
+for being used as reference code during the early stages of development.
+'''
 import asyncio
 import json
 from requests import get as get_url
 from datetime import datetime as dt
 
+import discord
+from discord.ext import commands
+
 #Bot token is stored in a separate file for secrecy
 from Token import token
-
 from Kumiko.bot_config import bot
-
-from Kumiko.commands import choose,help,imo,lemon,nusmods,pekopeko,poll,spam,suipiss
+from Kumiko.commands import choose,help,imo,lemon,nusmods,pekopeko,poll,spam,suipiss,test
 
 
 #https://stackoverflow.com/questions/62544309/why-client-emojis-newer-version-of-client-get-all-emojis-returns-empy-list-wh
 #https://stackoverflow.com/questions/71959420/client-init-missing-1-required-keyword-only-argument-intents
 client = discord.Client(intents=discord.Intents.default())
-
-
-
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#                           Bot commands
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Deleted commands: meta
-
-
-
-
-
-#Sample codes ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#Taken from 
-# https://github.com/Rapptz/discord.py/blob/v1.7.3/examples/basic_bot.py
 
 @bot.event
 async def on_ready():
@@ -43,49 +29,3 @@ async def on_ready():
     print('~' * 75)
 
 bot.run(token)
-
-# @bot.command()
-# async def add(ctx, left: int, right: int):
-#     """Adds two numbers together."""
-#     await ctx.send(left + right)
-
-# @bot.command()
-# async def roll(ctx, dice: str):
-#     """Rolls a dice in NdN format."""
-#     try:
-#         rolls, limit = map(int, dice.split('d'))
-#     except Exception:
-#         await ctx.send('Format has to be in NdN!')
-#         return
-
-#     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
-#     await ctx.send(result)
-
-# @bot.command(description='For when you wanna settle the score some other way')
-# async def choose(ctx, *choices: str):
-#     """Chooses between multiple choices."""
-#     await ctx.send(random.choice(choices))
-
-# @bot.command()
-# async def repeat(ctx, times: int, content='repeating...'):
-#     """Repeats a message multiple times."""
-#     for i in range(times):
-#         await ctx.send(content)
-
-# @bot.command()
-# async def joined(ctx, member: discord.Member):
-#     """Says when a member joined."""
-#     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-# @bot.group()
-# async def cool(ctx):
-#     """Says if a user is cool.
-#     In reality this just checks if a subcommand is being invoked.
-#     """
-#     if ctx.invoked_subcommand is None:
-#         await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-# @cool.command(name='bot')
-# async def _bot(ctx):
-#     """Is the bot cool?"""
-#     await ctx.send('Yes, the bot is cool.')
---
+++ Kumiko/testing.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext.commands import has_permissions
+from bot_config import bot, commands
+
+
+
+@bot.command()
+@has_permissions(administrator=True)
+async def test(ctx:commands.Context) -> None:
+    await ctx.channel.send('You are a discord mod :skull:')
+
+@test.error
+async def test_error(ctx:commands.Context, error:discord.ext.commands.errors) -> None:
+    await ctx.channel.send('Congrats you are not a discord mod (yet)')
+    await ctx.channel.send(error)
+    await ctx.channel.send(type(error))

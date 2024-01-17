98fa0d774f9dd350a4e49ad7bc0a2b57577187d4
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2024-01-17 21:49:50 +0800

    Remove spam messages from %spam

--- Kumiko/commands/spam.py
+++ Kumiko/commands/spam.py
@@ -7,13 +7,7 @@ from ..bot_config import bot, commands
 @bot.command()
 async def spam(ctx:commands.Context, n:int=5) -> None:
     '''h a'''
-    msges = ['OMG IT\'S A SUSSY AMOGUS BAKA-CHAN', 
-             'I\'ve been mixing my own urine, various perfumes and chemicals to make the perfect emulatuon of Hoshimachi Suisei\'s urine scent.', 
-             'Genshin Impact is a game which has sucked away my soul and killed my dog.', 
-             'Gura my dog died ***LET\'S GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO***',
-             'Meto mis juevos en tu *boca*',
-             'Genshin Impact is a game which has sucked away my soul and killed my dog',
-             'L + Ratio']
+    msges = ['TODO: Put some messages here']
     l = len(msges)
 
     #Prevent over-abuse

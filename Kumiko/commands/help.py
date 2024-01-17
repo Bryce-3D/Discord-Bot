75c2c4d9d1833fd6075ba9e796b1ef205d290311
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2024-01-17 21:51:03 +0800

    Implement part of %help

--- Kumiko/commands/help.py
+++ Kumiko/commands/help.py
@@ -1,7 +1,17 @@
 import discord
 from ..bot_config import bot, commands
 
-
+Kumiko_commands = [
+    'choose',
+    'help',
+    'imo',
+    'lemon',
+    'nusmods',
+    'pekopeko',
+    'poll',
+    'spam',
+    'suipiss',
+]
 
 @bot.command()
 async def help(ctx:commands.Context, cmd:str=None) -> None:
@@ -13,14 +23,34 @@ async def help(ctx:commands.Context, cmd:str=None) -> None:
     If cmd is provided, sends a detailed description of how to use %cmd
     '''
 
-    emb = discord.Embed(
-        title = '**Help**',
-        description = "Hi! My prefix is `%`",
-        color = 0xff6d01
-    )
-    emb.add_field(
-        name   = '__Commands__', 
-        value  = '> TODO',
-        inline = False
-    )
-    await ctx.channel.send(embed=emb)
+    if cmd == None:
+        s  = 'Hi! My prefix is `%`\n'
+        s += 'Use `%help cmd` for more detailed information on a specific command'
+        emb = discord.Embed(
+            title = '**Help**',
+            description = s,
+            color = 0xff6d01
+        )
+
+        s  = '> `%choose` - Choose an option from a given list of items\n'
+        s += '> `%help` - Used to display info about commands\n'
+        s += '> `%imo` - Used to send the link to a country\'s IMO page\n'
+        s += '> `%lemon` - Spams a lemon wave\n'
+        s += '> `%nusmods` - Used to look up information on modules in NUS through the NUSMods API\n'
+        s += '> `%pekopeko` - Generates a random string of Pekora\'s laugh\n'
+        s += '> `%poll` - Sends a poll in the form of a message\n'
+        s += '> `%spam` - Spams the chat\n'
+        s += '> `%suipiss` - Sends a link to a youtube video about suipiss'
+        emb.add_field(
+            name   = '__Commands__', 
+            value  = s,
+            inline = False
+        )
+        await ctx.channel.send(embed=emb)
+        return
+    
+    if cmd not in Kumiko_commands:
+        await ctx.send(f'I don\'t have any command called `{cmd}`')
+        return
+    
+    await ctx.send('TODO')

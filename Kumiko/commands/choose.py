6e446d71b98b9c04d69be08b65856683912a7769
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2024-01-17 21:35:48 +0800

    Add error message for %choose with 0 items

--- Kumiko/commands/choose.py
+++ Kumiko/commands/choose.py
@@ -6,4 +6,7 @@ from ..bot_config import bot, commands
 @bot.command()
 async def choose(ctx:commands.Context, *items:str) -> None:
     '''Prints a random item from a list of items'''
+    if len(items) == 0:
+        await ctx.send('Please put at least one option')
+        return
     await ctx.send(random.choice(items))

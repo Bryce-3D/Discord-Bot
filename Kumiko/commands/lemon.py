1fb75225c1073e05f579b0ea1a96a8c4d0d4cea5
Bryce-3D <94155427+Bryce-3D@users.noreply.github.com>
2024-01-17 11:31:29 +0800

    Fix lemon codestyle and slightly modify it

--- Kumiko/commands/lemon.py
+++ Kumiko/commands/lemon.py
@@ -1,32 +1,28 @@
+from time import sleep
 from ..bot_config import bot, commands
 
 #Adjustable parameters of the 🍋 wave
-cap   = 8
-curve = [0,1,4,7,8,7,4,1]
-width = 3
-count = 3
+curve = [0,1,4,7,8,7,4,1]   #Shape of the curve
+width = 3                   #Number of lemons per row
+count = 1                   #Number of unit waves per message
+limit = 100                 #Maximum number of messages to prevent abuse
 
 lemon_wave  = ''
 for i in curve:
     s  = ' '*i
-    s += '🍋'*3
-    s += ' '*(cap-i)
+    s += '🍋'*width
+    s += ' '*(max(curve)-i)
     s += '\n'
     lemon_wave += s
 
 lemon_wave *= count
 
-# lemon_wave  = '🍋🍋🍋      \n'
-# lemon_wave += '   🍋🍋🍋   \n'
-# lemon_wave += '      🍋🍋🍋\n'
-# lemon_wave += '   🍋🍋🍋   \n'
-# lemon_wave *= 5
-
 @bot.command()
 async def lemon(ctx:commands.Context, n:int=5) -> None:
     '''🍋 Lemon 🍋'''
     #Prevent over-abuse
-    n = min(n,100)
+    n = min(n,limit)
 
     for i in range(n):
         await ctx.send(lemon_wave)
+        sleep(1)
